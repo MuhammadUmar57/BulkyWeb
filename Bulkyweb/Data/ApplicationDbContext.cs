7227f598ca269bfc13c3eae0b7ad4ef7e43edba1
Muhammad Umar <muhammad.umar0516@gmail.com>
2023-05-22 17:13:19 +0500

    Push

---
+++ Bulkyweb/Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Bulkyweb.Models;
+using Microsoft.EntityFrameworkCore;
+//using System.Data.Entity;
+
+namespace Bulkyweb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories  { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Umar",
+                    DisplayOrder = 1,
+                },  
+                new Category
+                {
+                    Id = 2,
+                    Name = "Khan",
+                    DisplayOrder = 2,
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Muhammad",
+                    DisplayOrder = 3,
+                }
+
+                );
+        }
+    }
+}
